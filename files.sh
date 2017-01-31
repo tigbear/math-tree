a2d949f79ed9aa036292dcf087eb9cc09ab55d5e
Tanya Salyers <tanya.salyers@gmail.com>
2017-01-30 16:32:26 -0800

    Add java stub

--- files.sh
+++ files.sh
@@ -5,3 +5,10 @@ aws s3 ls tigbear/math/
 
 #aws db console
 https://us-west-2.console.aws.amazon.com/rds/home?region=us-west-2#dbinstances:
+
+#java stub
+mvn archetype:generate -DgroupId=com.geneology -DartifactId=math-tree -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+#java jar
+/Users/tanya/Code/math-tree/math-tree/target
+java -cp math-tree-1.0-SNAPSHOT.jar com.geneology.App
---
+++ math-tree/src/test/java/com/geneology/AppTest.java
@@ -0,0 +1,15 @@
+package com.geneology;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testOne() {
+        assertEquals(9000, 9000);
+    }
+}
