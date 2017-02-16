70bbd2793a924e6e142b86afab93a9167ea83d98
Tanya Salyers <tanya.salyers@gmail.com>
2017-02-15 16:22:07 -0800

    Save one file as json

--- files.sh
+++ files.sh
@@ -10,5 +10,7 @@ https://us-west-2.console.aws.amazon.com/rds/home?region=us-west-2#dbinstances:
 mvn archetype:generate -DgroupId=com.geneology -DartifactId=math-tree -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
 
 #java jar
-/Users/tanya/Code/math-tree/math-tree/target
-java -cp math-tree-1.0-SNAPSHOT.jar com.geneology.App
+#/Users/tanya/Code/math-tree/math-tree/target
+#java -cp math-tree-1.0-SNAPSHOT.jar com.geneology.App
+
+java -Din=/Users/tanya/CrawlResults/31332.html -Dout=/Users/tanya/ProcessedResults/31332.json -Dlog=/Users/tanya/Logs/ -cp target/math-tree-1.0-SNAPSHOT.jar com.geneology.App
--- math-tree/src/main/java/com/geneology/App.java
+++ math-tree/src/main/java/com/geneology/App.java
@@ -1,13 +1,29 @@
 package com.geneology;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import java.io.File;
+
 /**
- * Hello world!
+ * For all local html files, transform them into json blobs
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String in = System.getProperty("in");
+        String out = System.getProperty("out");
+        String log = System.getProperty("log");
+        System.out.println( in );
+        System.out.println( out );
+        System.out.println( log );
+        try {
+            MathematicianParser parser = new MathematicianParser();
+            Mathematician mathematician = parser.parse(in);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(new File(out), mathematician);
+        } catch( Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
